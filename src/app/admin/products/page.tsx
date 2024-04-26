32ac9cddeb5e51192dae240658ed60a64381f067
andrewgarrison <atg27@uw.edu>
2024-04-25 21:18:56 -0700

    create admin products page

---
+++ src/app/admin/products/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from '@/components/ui/button'
+import { PageHeader } from '../_components/pageHeader'
+import Link from 'next/link'
+
+export default function AdminProductsPage() {
+  return (
+    <>
+      <div className="flex justify-between items-center gap-4">
+        <PageHeader>Products</PageHeader>
+        <Button>
+          <Link href="/admin/products/new">Add Product</Link>
+        </Button>
+      </div>
+    </>
+  )
+}
